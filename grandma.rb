af98d31d3e2eaa634e5faab46ea89c291dc3f679
Martinique <me@martidolce.com>
2020-11-11 06:44:41 -0500

    commit tree 9e03f5d40a7cc9c723f727a535c7453964730914

--- grandma.rb
+++ grandma.rb
@@ -1,13 +1,31 @@
+# frozen_string_literal: true
+
 # Write a speak_to_grandma method.
 
 # Whatever you say to grandma, she should respond with
 # HUH?! SPEAK UP, SONNY!
 # unless you shout it (type in all capitals).
 
-# If you shout, she can hear you (or at least she thinks so) 
+# If you shout, she can hear you (or at least she thinks so)
 # and yells back
 
 # NO, NOT SINCE 1938!
 
 # However if you say 'I LOVE YOU GRANDMA!', she should respond with
 # 'I LOVE YOU TOO PUMPKIN!'
+
+#print 'Want to speak to grandma? '
+#answer = gets.chomp
+#
+def speak_to_grandma(response)
+	if response == 'I LOVE YOU GRANDMA!'
+		return 'I LOVE YOU TOO PUMPKIN!'
+	end
+	if response != response.upcase() || response == ''
+		return "HUH?! SPEAK UP, SONNY!"
+	else
+		return "NO, NOT SINCE 1938!"
+	end
+end
+
+puts speak_to_grandma('')
